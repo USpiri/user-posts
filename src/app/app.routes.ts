8dfbd46748241292c853ef792783a2074ae0bda1
USpiri <urielspiridione135@gmail.com>
2024-09-17 00:32:14 -0300

    ✨ Feat: Setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,47 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@shared/layouts/layout/layout.component';
 
 export const routes: Routes = [
   {
     path: 'posts',
-    title: 'Posts',
-    loadComponent: () =>
-      import('./features/posts/pages/posts/posts.component').then(
-        (m) => m.PostsComponent,
-      ),
-  },
-  {
-    path: 'posts/:id',
-    title: 'Post Form',
-    loadComponent: () =>
-      import('./features/posts/pages/post-form/post-form.component').then(
-        (m) => m.PostFormComponent,
-      ),
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        title: 'Posts',
+        loadComponent: () =>
+          import('./features/posts/pages/posts/posts.component').then(
+            (m) => m.PostsComponent,
+          ),
+      },
+      {
+        path: 'edit/:id',
+        title: 'Post Form',
+        loadComponent: () =>
+          import(
+            './features/posts/pages/post-form-page/post-form-page.component'
+          ).then((m) => m.PostFormPageComponent),
+      },
+      {
+        path: 'new',
+        title: 'Post Form',
+        loadComponent: () =>
+          import(
+            './features/posts/pages/post-form-page/post-form-page.component'
+          ).then((m) => m.PostFormPageComponent),
+      },
+    ],
   },
   {
     path: 'login',
     title: 'Login',
     loadComponent: () =>
       import('./features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: '',
+    redirectTo: '/posts',
+    pathMatch: 'full',
   },
   {
     path: '**',
