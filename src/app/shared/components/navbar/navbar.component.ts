95ce0e2668fe955c35570a2a76af49aa29e97e5d
USpiri <urielspiridione135@gmail.com>
2024-09-18 02:51:01 -0300

    🐛 Fix: Fixed navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { LucideAngularModule, LogOut } from 'lucide-angular';
   standalone: true,
   imports: [LucideAngularModule, RouterLink],
   template: `
-    <nav class="w-full bg-stone-100 shadow">
+    <nav class="w-full bg-stone-100 shadow fixed z-10 top-0">
       <div
         class="max-w-3xl mx-auto w-full flex px-4 py-3 justify-between *:flex *:items-center"
       >
