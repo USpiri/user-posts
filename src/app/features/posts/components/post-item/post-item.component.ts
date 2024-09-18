164aa520cca2ea4344452ac2d1d535bc0bdfd558
USpiri <urielspiridione135@gmail.com>
2024-09-18 02:54:06 -0300

    ✨ Feat: Get posts from server

--- src/app/features/posts/components/post-item/post-item.component.ts
+++ src/app/features/posts/components/post-item/post-item.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Post } from '@shared/models/post.interface';
 import { AuthService } from '@shared/services/auth.service';
 import { Edit, LucideAngularModule } from 'lucide-angular';
 
@@ -13,12 +14,16 @@ import { Edit, LucideAngularModule } from 'lucide-angular';
       class="border bg-neutral-50 px-3 py-1.5 border-neutral-300 w-full rounded-md relative justify-between flex"
     >
       <header>
-        <h2 class="font-semibold text-xl">Title</h2>
-        <p class="text-sm">Description</p>
+        <h2 class="font-semibold text-xl">{{ post.title }}</h2>
+        <p class="text-sm max-w-lg">{{ post.body }}</p>
       </header>
       @if ((user$ | async)?.role === 'admin') {
         <footer>
-          <a class="icon btn block" title="Edit post" routerLink="./edit/a">
+          <a
+            class="icon btn block"
+            title="Edit post"
+            routerLink="./edit/{{ post.id }}"
+          >
             <i-lucide [img]="edit" class="w-5 h-5" />
           </a>
         </footer>
@@ -27,7 +32,7 @@ import { Edit, LucideAngularModule } from 'lucide-angular';
   `,
 })
 export class PostItemComponent {
-  private auth = inject(AuthService);
-  user$ = this.auth.user$;
+  @Input({ required: true }) post!: Post;
+  user$ = inject(AuthService).user$;
   readonly edit = Edit;
 }
--- src/app/features/posts/components/post-list/post-list.component.ts
+++ src/app/features/posts/components/post-list/post-list.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { PostItemComponent } from '../post-item/post-item.component';
+import { PostService } from '@shared/services/post.service';
+import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [PostItemComponent],
+  imports: [PostItemComponent, LoaderComponent],
   template: `
-    <div class="flex flex-col gap-2">
-      <app-post-item />
-      <app-post-item />
-      <app-post-item />
-      <app-post-item />
-      <app-post-item />
-    </div>
+    @if (!postState().loading) {
+      <div class="flex flex-col gap-2">
+        @for (post of postState().posts; track post.id) {
+          <app-post-item [post]="post" />
+        } @empty {
+          <p>There is no posts to show</p>
+        }
+      </div>
+    } @else {
+      <app-loader />
+    }
   `,
-  styles: ``,
 })
-export class PostListComponent {}
+export class PostListComponent implements OnInit {
+  private postService = inject(PostService);
+  postState = this.postService.postState;
+
+  ngOnInit() {
+    // This only makes the request once to avoid resetting the loaded data
+    if (this.postState().posts.length === 0) this.postService.getPosts();
+  }
+}
