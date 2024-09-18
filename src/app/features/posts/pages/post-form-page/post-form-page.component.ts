253588c5347f1a3908f804bc8e614d91f5cd47ae
USpiri <urielspiridione135@gmail.com>
2024-09-18 02:50:21 -0300

    ✨ Feat: Change title depending on route

--- src/app/features/posts/pages/post-form-page/post-form-page.component.ts
+++ src/app/features/posts/pages/post-form-page/post-form-page.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SectionHeaderComponent } from '@shared/components/section-header/section-header.component';
 import { PostFormComponent } from '../../components/post-form/post-form.component';
 import { ChevronLeft, LucideAngularModule } from 'lucide-angular';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-post-form-page',
   standalone: true,
   template: `
     <main class="section">
-      <app-section-header title="Edit/Update Post">
+      <app-section-header [title]="isNew ? 'Create Post' : 'Edit Post'">
         <a routerLink="/" class="btn black text-sm flex items-center gap-2">
           <i-lucide [img]="back" class="w-5 h-5" />
           Back
@@ -29,5 +29,8 @@ import { RouterLink } from '@angular/router';
   ],
 })
 export class PostFormPageComponent {
+  private route = inject(ActivatedRoute);
+
   readonly back = ChevronLeft;
+  isNew = this.route.snapshot.routeConfig?.path === 'new';
 }
